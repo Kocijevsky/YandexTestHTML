ee0bb0076b026b7b7887214eb485d43b79185f35
Mike Kocijevsky <m.kocijevsky@gmail.com>
2014-01-08 16:08:42 +0200

    Scripts fix

--- modules/browsers/browsers.js
+++ modules/browsers/browsers.js
@@ -1,6 +1,6 @@
 var browsers = {};
 browsers.corruptImages = function(){
-    $(".browsers").find(".browsers_image").attr("src","")
+    $(".browsers").find(".browsers_image").attr("src","-")
 }
 
 $(".js_corrupt_images").on("click",
